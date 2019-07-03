0c212a53af5f8c64c0255d46fa7d364c65883886
vagrant <matias.tobar.c@usach.cl>
2019-07-03 23:59:44 +0000

    Auth generado

--- proyectodbd/routes/web.php
+++ proyectodbd/routes/web.php
@@ -48,3 +48,7 @@ Route::resource('tarjetaCredito', 'TarjetaCreditoController')->parameters(['tarj
 
 //Route::resource('/carrera/alumno/{id_alumno}', 'CarreraController@alumno');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
