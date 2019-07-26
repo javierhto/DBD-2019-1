67a3b145086db80936a5e87ad9c7911fc854792a
vagrant <felipe.villalobos.p@usach.cl>
2019-07-26 00:55:24 +0000

    Generacion automatica de horarios, parte 2

--- proyectodbd/resources/views/alumno/alumnoRamosAutomaticos.blade.php
+++ proyectodbd/resources/views/alumno/alumnoRamosAutomaticos.blade.php
@@ -65,7 +65,156 @@
                       </table> 
                       </center>
                       <br>
-                      Algunas opciones de horarios son:                      
+                      Algunas opciones de horarios son:
+
+
+
+
+                       <div class="table-responsive">
+                      <center>
+                        <table class="table table-bordered table-striped table-sm" id="dataTable" width="100%" cellspacing="0">
+                            <thead class="thead-dark text-center">
+                                <tr>
+                                    <td class="align-middle"> Bloques </td>
+                                    <td class="align-middle"> Lunes </td>
+                                    <td class="align-middle"> Martes </td>
+                                    <td class="align-middle"> Miercoles </td>
+                                    <td class="align-middle"> Jueves </td>
+                                    <td class="align-middle"> Viernes </td>
+                                    <td class="align-middle"> Sabado </td>
+                                </tr>
+                        </thead>
+                        <tbody class="text-center align-middle">
+                          @for($i = 1; $i<=9 ;$i++)
+                            <tr>
+
+                              <td class="align-middle"> Bloques {{ $i}} </td>
+                              <td class="align-middle">
+                                @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion && $horario->dia == 'lunes' && $horario->bloque == $i)
+                                            codigo:{{ $asignatura->id_asignatura }}
+                                            <br>
+                                            sala: {{ $horario->sala }} 
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+                              </td>
+
+
+                              <td class="align-middle">
+                                @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                      
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion)
+                                            @if($horario->dia == 'martes' && $horario->bloque == $i)
+                                              codigo:{{ $asignatura->id_asignatura }}
+                                              <br>
+                                              sala: {{ $horario->sala }} <br>                                  
+                                            @endif
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+
+                              </td>
+
+
+                              <td class="align-middle">
+                                  @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                      
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion)
+                                            @if($horario->dia == 'miercoles' && $horario->bloque == $i)
+                                              codigo:{{ $asignatura->id_asignatura }}
+                                              <br>
+                                              sala: {{ $horario->sala }} <br>                                  
+                                            @endif
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+                               </td>
+
+
+                              <td class="align-middle"> 
+                                @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                      
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion)
+                                            @if($horario->dia == 'jueves' && $horario->bloque == $i)
+                                              codigo:{{ $asignatura->id_asignatura }}
+                                              <br>
+                                              sala: {{ $horario->sala }} <br>                                  
+                                            @endif
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+
+                              </td>
+                              <td class="align-middle"> 
+                                @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                      
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion)
+                                            @if($horario->dia == 'viernes' && $horario->bloque == $i)
+                                              codigo:{{ $asignatura->id_asignatura }}
+                                              <br>
+                                              sala: {{ $horario->sala }} <br>                                  
+                                            @endif
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+                                  
+                               </td>
+                              <td class="align-middle"> 
+                                  @foreach ($asignaturas as $asignatura)
+                                  @foreach ($coordinaciones as $coordinacion)
+                                    @if($coordinacion->id_asignatura == $asignatura->id_asignatura)
+                                      
+                                        @foreach ($horarios as $horario)
+                                          @if($coordinacion->id == $horario->id_coordinacion)
+                                            @if($horario->dia == 'sabado' && $horario->bloque == $i)
+                                              codigo:{{ $asignatura->id_asignatura }}
+                                              <br>
+                                              sala: {{ $horario->sala }} <br>                                  
+                                            @endif
+                                          @endif
+                                        @endforeach
+                                    @endif
+                                  @endforeach
+                                @endforeach
+                               </td>
+
+                            </tr>
+                          @endfor
+                        </tbody>
+                      </table> 
+                      </center>
+                    </div>
+
+
+
+                      
+
                 </div>
         	</div>
     	</div>
